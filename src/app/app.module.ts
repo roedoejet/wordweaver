beb656db4ec6a0f98054a90dec8876f0b208efe9
roedoejet <aidanpine@shaw.ca>
2020-04-01 12:13:57 -0700

    fix(ux): added setting toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,87 @@
 // Core
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { MainApp } from './app.component';
-import { AboutPage, ConjPanel, ErrorSnackComponent, InfoPage, LogoAnime, PersPanel, SettingsComponent, TableviewerPage, TempPanel, VerbPanel, WordmakerPage, WordmakerConjPanel, WordmakerPersPanel, WordmakerTempPanel, WordmakerVerbPanel } from './components';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { MainApp } from "./app.component";
+import {
+  AboutPage,
+  ConjPanel,
+  ErrorSnackComponent,
+  InfoPage,
+  LogoAnime,
+  PersPanel,
+  SettingsComponent,
+  TableviewerPage,
+  TempPanel,
+  VerbPanel,
+  WordmakerPage,
+  WordmakerConjPanel,
+  WordmakerPersPanel,
+  WordmakerTempPanel,
+  WordmakerVerbPanel
+} from "./components";
 
 // Material Design
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
-  MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCheckboxModule, MatChipsModule, MatDialogModule, MatFormFieldModule,
-  MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatSnackBarModule, MatProgressSpinnerModule, MatRadioModule,
-  MatSelectModule, MatSlideToggleModule, MatStepperModule, MatTabsModule, MatToolbarModule, MatTooltipModule
-} from '@angular/material';
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatStepperModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from "@angular/material";
 
 // Services
-import { AffixService, AuthService, ChartService, ConjugationService, DatabaseService, PronounService, SelectionService, SettingsService, TierService, VerbService } from './services'
-import { HttpClientModule } from '@angular/common/http';
+import {
+  AffixService,
+  AuthService,
+  ChartService,
+  ConjugationService,
+  DatabaseService,
+  PronounService,
+  SelectionService,
+  SettingsService,
+  TierService,
+  VerbService
+} from "./services";
+import { HttpClientModule } from "@angular/common/http";
 
 // Pipes
-import { PipesModule } from './pipes/pipes.module';
+import { PipesModule } from "./pipes/pipes.module";
 
 // Firebase
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
-import { environment } from '../environments/environment';
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import {
+  AngularFireDatabaseModule,
+  AngularFireDatabase
+} from "angularfire2/database";
+import { environment } from "../environments/environment";
 
 // Extras
-import { NgxEchartsModule } from 'ngx-echarts';
+import { NgxEchartsModule } from "ngx-echarts";
+import { ToastrModule } from "ngx-toastr";
 
 export const routes = [
-  { path: '', redirectTo: 'wordmaker', pathMatch: 'full' },
+  { path: "", redirectTo: "wordmaker", pathMatch: "full" },
   // {
   //   path: 'wordmaker',
   //   component: MainApp,
@@ -41,10 +90,10 @@ export const routes = [
   //     { path: 'mobiledemo', component: MobiledemoComponent },
   //   ]
   // },
-  { path: 'wordmaker', component: WordmakerPage },
-  { path: 'tableviewer', component: TableviewerPage },
-  { path: 'info', component: TableviewerPage },
-  { path: 'about', component: AboutPage }
+  { path: "wordmaker", component: WordmakerPage },
+  { path: "tableviewer", component: TableviewerPage },
+  { path: "info", component: TableviewerPage },
+  { path: "about", component: AboutPage }
 ];
 
 @NgModule({
@@ -98,9 +147,14 @@ export const routes = [
     MatTooltipModule,
     MatToolbarModule,
     NgxEchartsModule,
+    ToastrModule.forRoot({
+      maxOpened: 1,
+      autoDismiss: true,
+      positionClass: "toast-top-center"
+    }),
     PipesModule,
     RouterModule.forRoot(routes),
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     AngularFireDatabase,
@@ -114,8 +168,9 @@ export const routes = [
     SelectionService,
     SettingsService,
     TierService,
-    VerbService],
+    VerbService
+  ],
   bootstrap: [MainApp],
   entryComponents: [ErrorSnackComponent, LogoAnime, SettingsComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,71 +1,89 @@
 // Core
-import { BehaviorSubject } from 'rxjs';
-import { Injectable } from '@angular/core';
-import { Level, Settings } from '../models'
-// import { ToastController } from 'ionic-angular';
+import { BehaviorSubject } from "rxjs";
+import { Injectable } from "@angular/core";
+import { Level, Settings } from "../models";
+import { ToastrService } from "ngx-toastr";
 
 // Services
-import { AuthService } from './auth.service'
-import { DatabaseService } from './database.service'
+import { AuthService } from "./auth.service";
+import { DatabaseService } from "./database.service";
 
 // Firebase
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase } from "angularfire2/database";
+
+// Meta
+import { MetaData } from "../global";
 
 @Injectable()
 export class SettingsService {
-    levels: Level[] = [{ 'profile': "advanced", "arg_label": "ag/pat", 'gloss_tier': false, 'english_tier': false, 'translation_tier': true, 'breakdown_tier': false },
-    { 'profile': "beginner", "arg_label": "sub/obj", 'gloss_tier': false, 'english_tier': false, 'translation_tier': true, 'breakdown_tier': false }]
-    help = new BehaviorSubject<Boolean>(false)
-    initial_settings: Settings = {
-        'level': this.levels[0],
-        'highlight': { 'root': false, 'pronoun': true, 'aspect': false, 'prepronominal_prefix': false, 'tmp_affix': false, 'post_aspectual_suffix': false },
-        'test_api': false
-    };
-    settings = new BehaviorSubject<Object>(this.initial_settings)
-    constructor(
-        private db: AngularFireDatabase, private dbService: DatabaseService,
-        private authService: AuthService, 
-        // private toastCtrl: ToastController
-    ) {
-        this.authService.user.subscribe(user => {
-            if (user) {
-                this.db.object('users/' + user.uid).valueChanges().subscribe((data) => {
-                    if (data) {
-                        this.settings.next(data['settings']);
-                    }
-                })
+  levels: Level[] = [
+    {
+      profile: "advanced",
+      arg_label: "ag/pat",
+      gloss_tier: false,
+      english_tier: false,
+      translation_tier: true,
+      breakdown_tier: false
+    },
+    {
+      profile: "beginner",
+      arg_label: "sub/obj",
+      gloss_tier: false,
+      english_tier: false,
+      translation_tier: true,
+      breakdown_tier: false
+    }
+  ];
+  help = new BehaviorSubject<Boolean>(false);
+  initial_settings: Settings = {
+    level: this.levels[0],
+    highlight: {
+      root: false,
+      pronoun: true,
+      aspect: false,
+      prepronominal_prefix: false,
+      tmp_affix: false,
+      post_aspectual_suffix: false
+    },
+    test_api: false
+  };
+  settings = new BehaviorSubject<Object>(this.initial_settings);
+  successMessage = MetaData.congratulations;
+  constructor(
+    private db: AngularFireDatabase,
+    private dbService: DatabaseService,
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) {
+    this.authService.user.subscribe(user => {
+      if (user) {
+        this.db
+          .object("users/" + user.uid)
+          .valueChanges()
+          .subscribe(data => {
+            if (data) {
+              this.settings.next(data["settings"]);
             }
-        })
+          });
+      }
+    });
+  }
 
+  updateToast(success?) {
+    if (success) {
+      this.toastr.success("Setting sucessfully updated", this.successMessage);
+    } else {
+      this.toastr.error(
+        "This setting couldn't be changed. Try disabling your adblocker for this site.",
+        "Whoops!"
+      );
     }
+  }
 
-    updateToast(success?) {
-        // if (success) {
-        //     let toast = this.toastCtrl.create({
-        //         message: 'Setting saved',
-        //         duration: 1000,
-        //         position: 'bottom'
-        //     });
-        //     toast.onDidDismiss(() => {
-        //         console.log('Dismissed toast');
-        //     });
-        //     toast.present();
-        // } else {
-        //     let toast = this.toastCtrl.create({
-        //         message: 'Warning: you are not logged in and this setting was not saved',
-        //         duration: 1000,
-        //         position: 'bottom'
-        //     });
-        //     toast.onDidDismiss(() => {
-        //         console.log('Dismissed toast');
-        //     });
-        //     toast.present();
-        // }
-    }
-
-    update(settings: Settings): void {
-        this.dbService.saveSettings(settings).then(result => { this.updateToast(result); console.log(result) })
-        this.settings.next(settings);
-    }
-
+  update(settings: Settings): void {
+    this.dbService.saveSettings(settings).then(result => {
+      this.updateToast(result);
+    });
+    this.settings.next(settings);
+  }
 }
