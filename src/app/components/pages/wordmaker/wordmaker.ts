603d2e74201e992f94406d8e793f6eb0f7072655
roedoejet <aidanpine@shaw.ca>
2020-04-01 11:54:09 -0700

    fix(ux): removed enter hotkey in wordmaker

--- src/app/components/pages/wordmaker/wordmaker.ts
+++ src/app/components/pages/wordmaker/wordmaker.ts
@@ -1,144 +1,189 @@
 // Core
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 // Models
-import { Verb, Conjugation, AffOption, Pronoun } from '../../../models'
+import { Verb, Conjugation, AffOption, Pronoun } from "../../../models";
 
 // Services
-import { ConjugationService, ChartService, VerbService, PronounService, AffixService } from '../../../services'
+import {
+  ConjugationService,
+  ChartService,
+  VerbService,
+  PronounService,
+  AffixService
+} from "../../../services";
 
 // Imports
-import { MetaData } from '../../../global'
-import * as anime from 'animejs';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
-import { EChartOption } from 'echarts';
-import { MatSnackBar } from '@angular/material';
-
+import { MetaData } from "../../../global";
+import * as anime from "animejs";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { EChartOption } from "echarts";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
-  selector: 'page-wordmaker',
-  templateUrl: 'wordmaker.html',
-  styleUrls: [
-    './wordmaker.scss'
-  ]
+  selector: "page-wordmaker",
+  templateUrl: "wordmaker.html",
+  styleUrls: ["./wordmaker.scss"]
 })
 export class WordmakerPage {
-  verbs: EChartOption
-  who: EChartOption
-  when: EChartOption
+  verbs: EChartOption;
+  who: EChartOption;
+  when: EChartOption;
   showVerbs: boolean = false;
   showWho: boolean = false;
   showWhen: boolean = false;
-  title = MetaData.title
-  currentStep = 0
+  title = MetaData.title;
+  currentStep = 0;
   selectedVerb: Verb;
   selectedPers: Pronoun;
   selectedTemp: AffOption;
   word$: any;
 
-  @HostListener('document:keyup', ['$event'])
+  @HostListener("document:keyup", ["$event"])
   handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.key == 'Enter' || event.key == 'ArrowRight') {
-      this.nextStep()
+    if (event.key == "ArrowRight") {
+      this.nextStep();
     }
-    if (event.key == 'ArrowLeft') {
-      this.previousStep()
+    if (event.key == "ArrowLeft") {
+      this.previousStep();
     }
   }
 
   tempSelected(temp: AffOption) {
-    this.selectedTemp = temp
-    this.finalStep()
+    this.selectedTemp = temp;
+    this.finalStep();
   }
 
   verbSelected(verb: Verb) {
-    console.log(verb)
-    this.selectedVerb = verb
-    this.secondStep()
+    console.log(verb);
+    this.selectedVerb = verb;
+    this.secondStep();
   }
 
   persSelected(pn) {
-    this.selectedPers = pn
-    if (this.selectedVerb.thematic_relation === 'red' && "agent" in pn) {
-      this.thirdStep()
-    } else if (this.selectedVerb.thematic_relation === 'blue' && "patient" in pn) {
-      this.thirdStep()
-    } else if (this.selectedVerb.thematic_relation === 'purple' && "agent" in pn && "patient" in pn) {
-      this.thirdStep()
+    this.selectedPers = pn;
+    if (this.selectedVerb.thematic_relation === "red" && "agent" in pn) {
+      this.thirdStep();
+    } else if (
+      this.selectedVerb.thematic_relation === "blue" &&
+      "patient" in pn
+    ) {
+      this.thirdStep();
+    } else if (
+      this.selectedVerb.thematic_relation === "purple" &&
+      "agent" in pn &&
+      "patient" in pn
+    ) {
+      this.thirdStep();
     } else {
     }
   }
 
-  constructor(private conjugationService: ConjugationService, private chartService: ChartService, private snackBar: MatSnackBar, private verbService: VerbService, private pronounService: PronounService, private affixService: AffixService) {
-  }
+  constructor(
+    private conjugationService: ConjugationService,
+    private chartService: ChartService,
+    private snackBar: MatSnackBar,
+    private verbService: VerbService,
+    private pronounService: PronounService,
+    private affixService: AffixService
+  ) {}
 
   randomX(x) {
-    return x[Math.floor((Math.random() * (x.length - 1)) + 1)]
+    return x[Math.floor(Math.random() * (x.length - 1) + 1)];
   }
 
   pickRandom(selection_type: string) {
-    if (selection_type === 'verb') {
-      return this.verbService.verbs$.pipe(
-        map(x => this.randomX(x))
-      )
-    } else if (selection_type === 'pronoun') {
-      if (this.selectedVerb.thematic_relation === 'red') {
+    if (selection_type === "verb") {
+      return this.verbService.verbs$.pipe(map(x => this.randomX(x)));
+    } else if (selection_type === "pronoun") {
+      if (this.selectedVerb.thematic_relation === "red") {
         return this.pronounService.pronouns$.pipe(
-          map(x => { return { 'agent': this.randomX(x) } })
-        )
-      } else if (this.selectedVerb.thematic_relation === 'blue') {
+          map(x => {
+            return { agent: this.randomX(x) };
+          })
+        );
+      } else if (this.selectedVerb.thematic_relation === "blue") {
         return this.pronounService.pronouns$.pipe(
-          map(x => { return { 'patient': this.randomX(x) } })
-        )
+          map(x => {
+            return { patient: this.randomX(x) };
+          })
+        );
       } else if (this.selectedVerb.thematic_relation === "purple") {
         return this.pronounService.pronouns$.pipe(
           map(pns => {
             let ag = this.randomX(pns);
-            let pats = pns.filter(x => x.person !== ag.person && !(ag.person === '2' && x.inclusivity === 'incl') && !(ag.inclusivity === 'incl' && x.person === '2'));
+            let pats = pns.filter(
+              x =>
+                x.person !== ag.person &&
+                !(ag.person === "2" && x.inclusivity === "incl") &&
+                !(ag.inclusivity === "incl" && x.person === "2")
+            );
             let pat = this.randomX(pats);
-            return { 'agent': ag, 'patient': pat }
-          }))
+            return { agent: ag, patient: pat };
+          })
+        );
       }
-    } else if (selection_type === 'temp') {
+    } else if (selection_type === "temp") {
       return this.affixService.affoptions$.pipe(
-        map(x => { return { 'affopt': this.randomX(x)['tag'] } })
-      )
+        map(x => {
+          return { affopt: this.randomX(x)["tag"] };
+        })
+      );
     }
   }
 
   showExplorer(el) {
-    if (el === 'verbs') {
-      this.chartService.returnChoiceAllData('verbs', undefined, 1).subscribe(r => { this.verbs = r; this.showVerbs = true });
-    } else if (el === 'who') {
-      this.chartService.returnChoiceAllData('pronouns', undefined, 1).subscribe(r => { this.who = r; this.showWho = true });
-    } else if (el === 'when') {
-      this.chartService.returnChoiceAllData('aff-options', undefined, 1).subscribe(r => { this.when = r; this.showWhen = true });
+    if (el === "verbs") {
+      this.chartService
+        .returnChoiceAllData("verbs", undefined, 1)
+        .subscribe(r => {
+          this.verbs = r;
+          this.showVerbs = true;
+        });
+    } else if (el === "who") {
+      this.chartService
+        .returnChoiceAllData("pronouns", undefined, 1)
+        .subscribe(r => {
+          this.who = r;
+          this.showWho = true;
+        });
+    } else if (el === "when") {
+      this.chartService
+        .returnChoiceAllData("aff-options", undefined, 1)
+        .subscribe(r => {
+          this.when = r;
+          this.showWhen = true;
+        });
     }
   }
 
   hideExplorer(el) {
-    if (el === 'verbs') {
-      this.chartService.returnChoiceAllData('verbs', undefined, 0).subscribe(r => {
-        this.verbs = r;
-        setTimeout(() => this.showVerbs = false, 500)
-      });
-    } else if (el === 'who') {
-      this.chartService.returnChoiceAllData('pronouns', undefined, 0).subscribe(r => {
-        this.who = r;
-        setTimeout(() => this.showWho = false, 500)
-      });
-    } else if (el === 'when') {
-      this.chartService.returnChoiceAllData('aff-options', undefined, 0).subscribe(r => {
-        this.when = r;
-        setTimeout(() => this.showWhen = false, 500)
-      });
+    if (el === "verbs") {
+      this.chartService
+        .returnChoiceAllData("verbs", undefined, 0)
+        .subscribe(r => {
+          this.verbs = r;
+          setTimeout(() => (this.showVerbs = false), 500);
+        });
+    } else if (el === "who") {
+      this.chartService
+        .returnChoiceAllData("pronouns", undefined, 0)
+        .subscribe(r => {
+          this.who = r;
+          setTimeout(() => (this.showWho = false), 500);
+        });
+    } else if (el === "when") {
+      this.chartService
+        .returnChoiceAllData("aff-options", undefined, 0)
+        .subscribe(r => {
+          this.when = r;
+          setTimeout(() => (this.showWhen = false), 500);
+        });
     }
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   ngAfterViewInit() {
     // this.startStep()
@@ -146,63 +191,75 @@ export class WordmakerPage {
 
   nextStep() {
     if (this.currentStep == 0) {
-      this.firstStep()
+      this.firstStep();
     } else if (this.currentStep == 1) {
       if (this.selectedVerb) {
-        this.secondStep()
+        this.secondStep();
       } else {
-        this.pickRandom('verb').subscribe(v => {
-          this.snackBar.open(`You didn't select anything, so the Word Maker picked '${v.gloss}'!`, "", { duration: 2500, verticalPosition: 'top' });
-          this.verbSelected(v)
-        })
+        this.pickRandom("verb").subscribe(v => {
+          this.snackBar.open(
+            `You didn't select anything, so the Word Maker picked '${v.gloss}'!`,
+            "",
+            { duration: 2500, verticalPosition: "top" }
+          );
+          this.verbSelected(v);
+        });
       }
     } else if (this.currentStep == 2) {
       if (this.selectedPers) {
-        this.thirdStep()
+        this.thirdStep();
       } else {
-        this.pickRandom('pronoun').subscribe(p => {
-          this.snackBar.open(`You didn't select anything, so the Word Maker picked for you!`, "", { duration: 2500, verticalPosition: 'top' });
-          this.persSelected(p)
-        })
+        this.pickRandom("pronoun").subscribe(p => {
+          this.snackBar.open(
+            `You didn't select anything, so the Word Maker picked for you!`,
+            "",
+            { duration: 2500, verticalPosition: "top" }
+          );
+          this.persSelected(p);
+        });
       }
     } else if (this.currentStep == 3) {
       if (this.selectedTemp) {
-        this.finalStep()
+        this.finalStep();
       } else {
-        this.pickRandom('temp').subscribe(t => {
-          this.snackBar.open(`You didn't select anything, so the Word Maker picked for you!`, "", { duration: 2500, verticalPosition: 'top' });
-          this.tempSelected(t)
-        })
+        this.pickRandom("temp").subscribe(t => {
+          this.snackBar.open(
+            `You didn't select anything, so the Word Maker picked for you!`,
+            "",
+            { duration: 2500, verticalPosition: "top" }
+          );
+          this.tempSelected(t);
+        });
       }
     }
   }
 
-  triggeredPreviousStep(start?){
-    if (start){
-      this.backToFirstStep()
+  triggeredPreviousStep(start?) {
+    if (start) {
+      this.backToFirstStep();
     } else {
-      this.previousStep()
+      this.previousStep();
     }
   }
 
   previousStep() {
     if (this.currentStep == 4) {
-      this.backToThirdStep()
+      this.backToThirdStep();
     } else if (this.currentStep == 3) {
-      this.backToSecondStep()
+      this.backToSecondStep();
     } else if (this.currentStep == 2) {
-      this.backToFirstStep()
+      this.backToFirstStep();
     } else {
-      this.backToStartStep()
+      this.backToStartStep();
     }
   }
 
   startStep() {
-    this.currentStep = 0
+    this.currentStep = 0;
   }
 
   backToStartStep() {
-    this.currentStep = 0
+    this.currentStep = 0;
   }
 
   firstStep() {
@@ -210,14 +267,14 @@ export class WordmakerPage {
       targets: "#welcome",
       translateX: 5000,
       duration: 1000,
-      easing: 'easeInOutQuad',
+      easing: "easeInOutQuad",
       complete: () => {
-        this.currentStep = 1
+        this.currentStep = 1;
         anime({
           targets: "#firstStep div mat-chip-list div mat-chip",
           translateX: 50,
           duration: 250,
-          easing: 'easeInOutQuad'
+          easing: "easeInOutQuad"
         });
       }
     });
@@ -227,18 +284,18 @@ export class WordmakerPage {
     this.selectedVerb = null;
     this.selectedPers = null;
     this.selectedTemp = null;
-    this.currentStep = 1
+    this.currentStep = 1;
   }
 
   backToSecondStep() {
     this.selectedPers = null;
     this.selectedTemp = null;
-    this.currentStep = 2
+    this.currentStep = 2;
   }
 
   backToThirdStep() {
     this.selectedTemp = null;
-    this.currentStep = 3
+    this.currentStep = 3;
   }
 
   secondStep() {
@@ -247,15 +304,17 @@ export class WordmakerPage {
       translateX: 1200,
       duration: 1000,
       opacity: 0,
-      delay: function (el, i) { return i * 5 },
-      easing: 'easeInOutQuad',
+      delay: function(el, i) {
+        return i * 5;
+      },
+      easing: "easeInOutQuad",
       complete: () => {
-        this.currentStep = 2
+        this.currentStep = 2;
         anime({
           targets: "#secondStep",
           translateX: 3000,
           duration: 1500,
-          easing: 'easeInOutQuad'
+          easing: "easeInOutQuad"
         });
       }
     });
@@ -267,62 +326,69 @@ export class WordmakerPage {
       translateX: 1200,
       duration: 1000,
       opacity: 0,
-      delay: function (el, i) { return i * 5 },
-      easing: 'easeInOutQuad',
+      delay: function(el, i) {
+        return i * 5;
+      },
+      easing: "easeInOutQuad",
       complete: () => {
-        this.currentStep = 3
+        this.currentStep = 3;
         anime({
           targets: "#thirdStep",
           translateX: 3000,
           duration: 1500,
-          easing: 'easeInOutQuad'
+          easing: "easeInOutQuad"
         });
       }
     });
   }
 
   finalStep() {
-    let ag_tag = []
-    let pat_tag = []
+    let ag_tag = [];
+    let pat_tag = [];
     if ("agent" in this.selectedPers) {
-      ag_tag.push(this.selectedPers['agent']['tag'])
+      ag_tag.push(this.selectedPers["agent"]["tag"]);
     }
     if ("patient" in this.selectedPers) {
-      pat_tag.push(this.selectedPers['patient']['tag'])
+      pat_tag.push(this.selectedPers["patient"]["tag"]);
     }
-    let vb_tag = this.selectedVerb['tag']
-    let ao_tag = this.selectedTemp['affopt']
-    let table_args = { "agent": ag_tag, "patient": pat_tag, "root": [vb_tag], "aff-option": [ao_tag] }
-    console.log(table_args)
-    this.word$ = this.conjugationService.conjugateTable(table_args)
+    let vb_tag = this.selectedVerb["tag"];
+    let ao_tag = this.selectedTemp["affopt"];
+    let table_args = {
+      agent: ag_tag,
+      patient: pat_tag,
+      root: [vb_tag],
+      "aff-option": [ao_tag]
+    };
+    console.log(table_args);
+    this.word$ = this.conjugationService.conjugateTable(table_args);
     anime({
       targets: "#thirdStep div div mat-chip-list div mat-chip",
       translateX: 1200,
       duration: 1000,
       opacity: 0,
-      delay: function (el, i) { return i * 5 },
-      easing: 'easeInOutQuad',
+      delay: function(el, i) {
+        return i * 5;
+      },
+      easing: "easeInOutQuad",
       complete: () => {
-        this.currentStep = 4
+        this.currentStep = 4;
       }
     });
-
   }
-
 
   flip(ev: Event, verb) {
     if (!verb.flipped) {
       anime({
         targets: ev.target,
-        rotateX: ['0', '360'],
+        rotateX: ["0", "360"],
         // translateX: 5000,
         duration: 800,
-        easing: 'easeInOutQuad',
+        easing: "easeInOutQuad",
         update: () => {
           setTimeout(() => {
             verb.flipped = true;
-          }, 400)
-        },
+          }, 400);
+        }
       });
     }
   }
@@ -330,17 +396,16 @@ export class WordmakerPage {
   unflip(ev, verb) {
     anime({
       targets: ev.target,
-      rotateX: ['360', '0'],
+      rotateX: ["360", "0"],
       // translateX: 5000,
       duration: 800,
-      easing: 'easeInOutQuad',
+      easing: "easeInOutQuad",
       update: () => {
         setTimeout(() => {
           verb.flipped = false;
-        }, 400)
+        }, 400);
       }
     });
     // }
   }
-
 }
