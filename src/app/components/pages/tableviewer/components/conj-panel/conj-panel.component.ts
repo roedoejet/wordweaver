f8c47ec02b9745c6e604919cc73bdaa540e6c323
roedoejet <aidanpine@shaw.ca>
2020-04-01 16:55:57 -0700

    fix(docx): error messaging with docx

--- src/app/components/pages/tableviewer/components/conj-panel/conj-panel.component.ts
+++ src/app/components/pages/tableviewer/components/conj-panel/conj-panel.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Conjugation, Settings } from '../../../../../models'
-import { Observable } from 'rxjs'
-import { SettingsService, ChartService, ConjugationService, SelectionService } from '../../../../../services'
-import { MetaData } from '../../../../../global'
-import { EChartOption } from 'echarts';
+import { Component, OnInit, Input, ViewChild } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Conjugation, Settings } from "../../../../../models";
+import { Observable } from "rxjs";
+import {
+  SettingsService,
+  ChartService,
+  ConjugationService,
+  SelectionService
+} from "../../../../../services";
+import { MetaData } from "../../../../../global";
+import { EChartOption } from "echarts";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'conj-panel',
-  templateUrl: './conj-panel.component.html',
-  styleUrls: ['./conj-panel.component.scss',
-    '../../tableviewer.scss']
+  selector: "conj-panel",
+  templateUrl: "./conj-panel.component.html",
+  styleUrls: ["./conj-panel.component.scss", "../../tableviewer.scss"]
 })
 export class ConjPanel implements OnInit {
   chart_response$: Observable<EChartOption>;
   chart_response: EChartOption;
   response$: any;
-  @ViewChild('explorer') explorer;
+  @ViewChild("explorer") explorer;
   showDelay = new FormControl(1000);
   hideDelay = new FormControl(200);
-  tooltipPosition = 'above';
+  tooltipPosition = "above";
   settings: Settings;
   language = MetaData.language;
   showExplorer = false;
@@ -27,36 +33,48 @@ export class ConjPanel implements OnInit {
   show_toolbar = true;
   order = true;
   depth = 1;
-  constructor(private settingsService: SettingsService, private conjugationService: ConjugationService,
-    private selectionService: SelectionService, private chartService: ChartService) {
-  }
+  successMessage = MetaData.congratulations;
+  constructor(
+    private settingsService: SettingsService,
+    private conjugationService: ConjugationService,
+    private selectionService: SelectionService,
+    private chartService: ChartService,
+    private http: HttpClient,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
-    this.settingsService.settings.subscribe((settings: Settings) => { this.settings = settings; });
-  }
-
-  ngOnChanges() {
-
+    this.settingsService.settings.subscribe((settings: Settings) => {
+      this.settings = settings;
+    });
   }
 
   conjugate() {
     this.loading = true;
     if (!this.showExplorer) {
-      this.response$ = this.conjugationService.conjugateTable(this.selectionService.selection);
+      this.response$ = this.conjugationService.conjugateTable(
+        this.selectionService.selection
+      );
       console.log(this.response$);
       this.response$.subscribe(x => console.log(x));
       return this.response$;
     } else {
-      let order = 'PT';
+      let order = "PT";
       if (this.order) {
-        order = 'TP';
+        order = "TP";
       }
-      this.chart_response$ = this.chartService.createChart(this.selectionService.selection, order, this.depth)
-      this.chart_response$.subscribe(r => this.chart_response = r);
+      this.chart_response$ = this.chartService.createChart(
+        this.selectionService.selection,
+        order,
+        this.depth
+      );
+      this.chart_response$.subscribe(r => (this.chart_response = r));
     }
   }
 
-  isString(val) { return typeof val === 'string'; }
+  isString(val) {
+    return typeof val === "string";
+  }
 
   toggleExplorer() {
     this.showExplorer = !this.showExplorer;
@@ -64,11 +82,37 @@ export class ConjPanel implements OnInit {
   }
 
   download() {
-    console.log('test');
-    const query_args = this.conjugationService.createRequestUrl(this.selectionService.selection,
-      [{ 'param': 'docx', 'value': 'true' }]).toString();
-    const url = this.conjugationService.path + '?' + query_args;
-    window.location.href = url;
+    const query_args = this.conjugationService
+      .createRequestUrl(this.selectionService.selection)
+      .toString();
+    const query_args_docx = this.conjugationService
+      .createRequestUrl(this.selectionService.selection, [
+        { param: "docx", value: "true" }
+      ])
+      .toString();
+    const url = this.conjugationService.path + "?" + query_args;
+    const docx_url = this.conjugationService.path + "?" + query_args_docx;
+    this.http.get(url).subscribe(
+      data => {
+        console.log(data);
+        window.location.href = docx_url;
+        this.updateToast(data);
+      },
+      error => {
+        console.log(error);
+        this.updateToast(false, error.status);
+      }
+    );
   }
 
+  updateToast(success?, code = 200) {
+    if (success) {
+      this.toastr.success("File succesfully downloadd", this.successMessage);
+    } else {
+      this.toastr.error(
+        "This file couldn't be downloaded. Please make sure your conjugation is valid.",
+        "Whoops!"
+      );
+    }
+  }
 }
--- src/app/components/pages/tableviewer/tableviewer.ts
+++ src/app/components/pages/tableviewer/tableviewer.ts
@@ -1,58 +1,51 @@
 // Core
-import { Component, HostListener, ViewChild } from '@angular/core';
-import { HttpParams } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Component, HostListener, ViewChild, OnInit } from "@angular/core";
 
 // Imports
-import { ChartService, ConjugationService, SelectionService } from '../../../services';
-import { Observable } from 'rxjs';
-import { ConjPanel } from './components/conj-panel/conj-panel.component'
-import { Conjugation } from '../../../models'
+import { ConjPanel } from "./components/conj-panel/conj-panel.component";
+
 @Component({
-  selector: 'page-tableviewer',
-  templateUrl: 'tableviewer.html',
-  styleUrls: [
-    './tableviewer.scss']
+  selector: "page-tableviewer",
+  templateUrl: "tableviewer.html",
+  styleUrls: ["./tableviewer.scss"]
 })
-export class TableviewerPage {
-  @HostListener('document:keypress', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.keyCode == 13) {
-      this.conjPanel.conjugate()
-    }
-  }
-  @ViewChild('conjPanel') conjPanel: ConjPanel;
+export class TableviewerPage implements OnInit {
+  @ViewChild("conjPanel") conjPanel: ConjPanel;
   cols: number;
   rows: number;
   conjrowspan: number;
   conjcolspan: number;
   halfrowspan: number;
   fullrowspan: number;
-  breakpoint: number = 1100;
-  useTestAPI: boolean = false;
-  constructor() {
+  breakpoint = 1100;
+  useTestAPI = false;
+  @HostListener("document:keypress", ["$event"])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.keyCode === 13) {
+      this.conjPanel.conjugate();
+    }
   }
+  constructor() {}
 
   ngOnInit() {
-    this.cols = (window.innerWidth <= this.breakpoint) ? 3 : 12
-    this.rows = (window.innerWidth <= this.breakpoint) ? 1 : 2
-    this.conjcolspan = (window.innerWidth <= this.breakpoint) ? 3 : 6
-    this.conjrowspan = (window.innerWidth <= this.breakpoint) ? 1 : 2
-    this.halfrowspan = (window.innerWidth <= this.breakpoint) ? 1 : 1
-    this.fullrowspan = (window.innerWidth <= this.breakpoint) ? 1 : 2
+    this.cols = window.innerWidth <= this.breakpoint ? 3 : 12;
+    this.rows = window.innerWidth <= this.breakpoint ? 1 : 2;
+    this.conjcolspan = window.innerWidth <= this.breakpoint ? 3 : 6;
+    this.conjrowspan = window.innerWidth <= this.breakpoint ? 1 : 2;
+    this.halfrowspan = window.innerWidth <= this.breakpoint ? 1 : 1;
+    this.fullrowspan = window.innerWidth <= this.breakpoint ? 1 : 2;
   }
 
   onResize(event) {
-    this.cols = (event.target.innerWidth <= this.breakpoint) ? 3 : 12;
-    this.rows = (window.innerWidth <= this.breakpoint) ? 1 : 2
-    this.conjcolspan = (window.innerWidth <= this.breakpoint) ? 3 : 6
-    this.conjrowspan = (window.innerWidth <= this.breakpoint) ? 1 : 2
-    this.halfrowspan = (window.innerWidth <= this.breakpoint) ? 1 : 1
-    this.fullrowspan = (window.innerWidth <= this.breakpoint) ? 1 : 2
+    this.cols = event.target.innerWidth <= this.breakpoint ? 3 : 12;
+    this.rows = window.innerWidth <= this.breakpoint ? 1 : 2;
+    this.conjcolspan = window.innerWidth <= this.breakpoint ? 3 : 6;
+    this.conjrowspan = window.innerWidth <= this.breakpoint ? 1 : 2;
+    this.halfrowspan = window.innerWidth <= this.breakpoint ? 1 : 1;
+    this.fullrowspan = window.innerWidth <= this.breakpoint ? 1 : 2;
   }
 
-  conjugate(){
+  conjugate() {
     this.conjPanel.conjugate();
   }
-
 }
