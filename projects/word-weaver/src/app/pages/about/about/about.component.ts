6e653f1b1a6458daee09f5001f209162a29535a3
roedoejet <aidanpine@shaw.ca>
2020-04-09 18:50:55 -0700

    fix(cleanup): removed links to deleted images

--- projects/word-weaver/src/app/pages/about/about/about.component.ts
+++ projects/word-weaver/src/app/pages/about/about/about.component.ts
@@ -10,8 +10,6 @@ import { ROUTE_ANIMATIONS_ELEMENTS } from "../../../core/core.module";
 })
 export class AboutComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
-  releaseButler = require("../../../../assets/release-butler.png");
-
   constructor() {}
 
   ngOnInit() {}
