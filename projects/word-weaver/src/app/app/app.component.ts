dfd7102f724cba8ab6b2afca449a39cd95a125a4
roedoejet <aidanpine@shaw.ca>
2020-04-23 13:41:54 -0700

    fix(i18n): fix display value for language

--- projects/word-weaver/src/app/app/app.component.ts
+++ projects/word-weaver/src/app/app/app.component.ts
@@ -34,7 +34,10 @@ export class AppComponent implements OnInit {
   version = env.versions.app;
   year = new Date().getFullYear();
   logo = require("../../assets/logo.png");
-  languages = ["en", "fr"];
+  languages = [
+    { label: "en", value: "en" },
+    { label: "fr", value: "fr" }
+  ];
   navigation = [
     { link: "wordmaker", label: marker("ww.menu.wordmaker") },
     { link: "tableviewer", label: marker("ww.menu.tableviewer") },
