8c058d4cf29e3843c09f5cf59b87efb11cf26cee
roedoejet <aidanpine@shaw.ca>
2020-05-01 13:06:20 -0700

    fix(i18n): added highlights

--- projects/word-weaver/src/app/i18n/i18n.ts
+++ projects/word-weaver/src/app/i18n/i18n.ts
@@ -7,3 +7,15 @@ marker("ww.settings.general.tiers.breakdown");
 marker("ww.settings.general.tiers.gloss");
 marker("ww.settings.general.tiers.breakdown-translation");
 marker("ww.settings.general.tiers.translation");
+
+// Tiers
+// Moh
+marker("ww.settings.general.highlighting.aspect");
+marker("ww.settings.general.highlighting.post-aspectual");
+marker("ww.settings.general.highlighting.pronouns");
+marker("ww.settings.general.highlighting.required");
+marker("ww.settings.general.highlighting.temp");
+marker("ww.settings.general.highlighting.verb");
+// Fr
+marker("ww.settings.general.highlighting.ending");
+marker("ww.settings.general.highlighting.root");
