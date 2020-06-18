306c9cbf40df968d7c79a8d72b15ae9563670245
roedoejet <aidanpine@shaw.ca>
2020-06-18 10:47:18 -0700

    fix(storage): slugifed env base name as pfx to ls to avoid ns collision

--- projects/word-weaver/src/app/core/local-storage/local-storage.service.ts
+++ projects/word-weaver/src/app/core/local-storage/local-storage.service.ts
@@ -1,6 +1,34 @@
 import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
 
-const APP_PREFIX = "ww-";
+// Slugify a string
+function slugify(str) {
+  str = str.replace(/^\s+|\s+$/g, "");
+
+  // Make the string lowercase
+  str = str.toLowerCase();
+
+  // Remove accents, swap ñ for n, etc
+  var from =
+    "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;";
+  var to =
+    "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
+
+  // Remove invalid chars
+  str = str
+    .replace(/[^a-z0-9 -]/g, "")
+    // Collapse whitespace and replace by -
+    .replace(/\s+/g, "-")
+    // Collapse dashes
+    .replace(/-+/g, "-");
+
+  return str;
+}
+
+const APP_PREFIX = "ww-" + slugify(environment.base);
 
 @Injectable({
   providedIn: "root"
